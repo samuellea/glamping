e80f00af6d19f4d954cfd1279507dd281b2e3b5e
samuellea <samuel.lea@live.co.uk>
2019-10-21 05:55:59 +0100

    Tweaked stylings and layouts 3

--- src/components/Error.js
+++ src/components/Error.js
@@ -9,10 +9,14 @@ const Error = ({ error }) => {
   )
 
   return (
-    <div>
-      <h1 style={{ fontSize: '50px' }}>404 <span style={{ color: 'darkred' }}>:(</span></h1>
-      <p>Page not found!</p>
-    </div>
+    <>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <h1 style={{ fontSize: '100px', paddingTop: '100px', color: "#4C2A66" }}>404 <span style={{ color: '#9f5ad5' }}>:(</span></h1>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center', padding: '0% 5%' }} >
+        <p style={{ fontSize: '25px', color: 'gray' }}>Whatever was here hit the trail some time ago...</p>
+      </div>
+    </>
   )
 
 };
