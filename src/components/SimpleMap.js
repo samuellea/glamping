0d67dcb43b543229602e2aa88c23e133bafc802c
samuellea <samuel.lea@live.co.uk>
2021-01-12 10:54:07 +0000

    Switched out google-maps-react for @react-google-maps/api

--- src/components/SimpleMap.js
+++ src/components/SimpleMap.js
@@ -1,92 +1,52 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 
-class SimpleMap extends Component {
-  render() {
-    console.log(process.env.REACT_APP_GOOGLE_MAPS)
-    const { lat, long } = this.props;
-    const mapStyles = {
-      width: '100%',
-      height: '100%'
-    }
-    return (
-      <>
-        {
-          this.props.lat !== null ?
-            <Map
-              className="simpleMap"
-              google={this.props.google}
-              zoom={8}
-              style={mapStyles}
-              initialCenter={{ lat: lat, lng: long }}
-            >
-              <Marker position={{ lat: lat, lng: long }} style={{ color: 'pink' }} />
-            </Map>
-            : null
-        }
-      </>
-    );
+
+function MyComponent(props) {
+
+  const containerStyle = {
+    width: '100vw',
+    height: '50vh'
   }
+
+  const center = {
+    lat: props.lat,
+    lng: props.long
+  };
+
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: 'AIzaSyCjrB3w6LNwgCcL_MBfIyL7sFTw15aqeaE'
+  })
+
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      <Marker position={center} />
+      { /* Child components, such as markers, info windows, etc. */}
+      <></>
+    </GoogleMap>
+  ) : <></>
 }
 
-export default GoogleApiWrapper({
-  apiKey: 'AIzaSyCjrB3w6LNwgCcL_MBfIyL7sFTw15aqeaE'
-})(SimpleMap);
+export default React.memo(MyComponent)
 
-// import React, { Component } from 'react';
-// import GoogleMapReact from 'google-map-react';
 
-// const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
-// class SimpleMap extends Component {
-
-//   state = {
-//     center: {
-//       lat: '54.400864',
-//       long: '-2.96611'
-//     }
-//   }
-
-//   static defaultProps = {
-//     center: {
-//       lat: this.props.lat,
-//       lng: this.props.long
-//     },
-//     zoom: 11
-//   };
-
-//   render() {
-//     console.log(this.props, '<---')
-//     return (
-//       // Important! Always set the container height explicitly
-//       <div style={{ height: '50vh', width: '50%' }}>
-//         <GoogleMapReact
-//           bootstrapURLKeys={{ key: 'AIzaSyABmoMZcZje1cywQwARC6Xv9rw0KMYiFPQ' }}
-//           defaultCenter={this.props.center}
-//           defaultZoom={this.props.zoom}
-//         // center={this.state.center}
-//         >
-//           <AnyReactComponent
-//             lat={this.state.center.lat}
-//             lng={this.state.center.long}
-//             text="My Marker"
-//           />
-//         </GoogleMapReact>
-//       </div>
-//     );
-//   }
-
-//   componentDidMount() {
-//     const { lat, long } = this.props;
-//     console.log(lat);
-//     console.log(long, '*');
-//     this.setState({
-//       center: {
-//         lat: lat,
-//         long: long
-//       }
-//     })
-//   }
-// }
-
-// export default SimpleMap;
+//   apiKey: 'AIzaSyCjrB3w6LNwgCcL_MBfIyL7sFTw15aqeaE'
---
+++ src/components/SimpleMapOld.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+
+class SimpleMap extends Component {
+  render() {
+    console.log(process.env.REACT_APP_GOOGLE_MAPS)
+    const { lat, long } = this.props;
+    const mapStyles = {
+      width: '100%',
+      height: '100%'
+    }
+    return (
+      <>
+        {
+          this.props.lat !== null ?
+            <Map
+              className="simpleMap"
+              google={this.props.google}
+              zoom={8}
+              style={mapStyles}
+              initialCenter={{ lat: lat, lng: long }}
+            >
+              <Marker position={{ lat: lat, lng: long }} style={{ color: 'pink' }} />
+            </Map>
+            : null
+        }
+      </>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyCjrB3w6LNwgCcL_MBfIyL7sFTw15aqeaE'
+})(SimpleMap);
+
+// import React, { Component } from 'react';
+// import GoogleMapReact from 'google-map-react';
+
+// const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+// class SimpleMap extends Component {
+
+//   state = {
+//     center: {
+//       lat: '54.400864',
+//       long: '-2.96611'
+//     }
+//   }
+
+//   static defaultProps = {
+//     center: {
+//       lat: this.props.lat,
+//       lng: this.props.long
+//     },
+//     zoom: 11
+//   };
+
+//   render() {
+//     console.log(this.props, '<---')
+//     return (
+//       // Important! Always set the container height explicitly
+//       <div style={{ height: '50vh', width: '50%' }}>
+//         <GoogleMapReact
+//           bootstrapURLKeys={{ key: 'AIzaSyABmoMZcZje1cywQwARC6Xv9rw0KMYiFPQ' }}
+//           defaultCenter={this.props.center}
+//           defaultZoom={this.props.zoom}
+//         // center={this.state.center}
+//         >
+//           <AnyReactComponent
+//             lat={this.state.center.lat}
+//             lng={this.state.center.long}
+//             text="My Marker"
+//           />
+//         </GoogleMapReact>
+//       </div>
+//     );
+//   }
+
+//   componentDidMount() {
+//     const { lat, long } = this.props;
+//     console.log(lat);
+//     console.log(long, '*');
+//     this.setState({
+//       center: {
+//         lat: lat,
+//         long: long
+//       }
+//     })
+//   }
+// }
+
+// export default SimpleMap;
