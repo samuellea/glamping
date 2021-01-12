6cbbdc52f268d2c895e2baec1ce7b11ce2d734ad
samuellea <samuel.lea@live.co.uk>
2021-01-12 11:08:58 +0000

    changed api.postcodes.io axios instance to use https instead of http - was causing security issue

--- src/components/api.js
+++ src/components/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // const request1 = axios.create({ baseURL: 'http://localhost:8080/' })
 const request1 = axios.create({ baseURL: 'https://my-json-server.typicode.com/samuellea/json-glamping' })
-const request2 = axios.create({ baseURL: 'http://api.postcodes.io/postcodes/' })
+const request2 = axios.create({ baseURL: 'https://api.postcodes.io/postcodes/' })
 
 export const getResults = (location, checkIn, checkOut) => {
   return request1.get('/sites').then(({ data }) => {
