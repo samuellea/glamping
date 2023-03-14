405877f5bd25c52457c3f614bba935004bd48f74
samuellea <samuel.lea@live.co.uk>
2023-03-14 20:55:43 +0000

    Updates

---
+++ precache-manifest.739bddf7514ea04783f93e210d2a23d4.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bab8119bf7cfabf3865f7a54ae94e5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8df4c722ec1bc67fb63c",
+    "url": "/static/css/2.c6231af7.chunk.css"
+  },
+  {
+    "revision": "1863562f58291d96ef63",
+    "url": "/static/css/main.b5c60db3.chunk.css"
+  },
+  {
+    "revision": "8df4c722ec1bc67fb63c",
+    "url": "/static/js/2.d6669e7f.chunk.js"
+  },
+  {
+    "revision": "1863562f58291d96ef63",
+    "url": "/static/js/main.276d8998.chunk.js"
+  },
+  {
+    "revision": "643a60161dcd2a47e921",
+    "url": "/static/js/runtime-main.27468b01.js"
+  },
+  {
+    "revision": "0acb9b519ac3c8110bed99dcdf088eb5",
+    "url": "/static/media/Caravan.0acb9b51.jpeg"
+  },
+  {
+    "revision": "f4405c52b257bfcc834af4c3c88470bc",
+    "url": "/static/media/Commune.f4405c52.jpeg"
+  },
+  {
+    "revision": "657d0968a88305db685ff740d42b8b5e",
+    "url": "/static/media/Yurt.657d0968.jpeg"
+  },
+  {
+    "revision": "7e137835955cd89597c873c2832c7d4c",
+    "url": "/static/media/arts.7e137835.jpg"
+  },
+  {
+    "revision": "3a9e0aedb9ddb0270cdda9a3e31d3fa9",
+    "url": "/static/media/camping-tent-purp.3a9e0aed.svg"
+  },
+  {
+    "revision": "50635b1b941fa14f2bee9e7e0ebdd92c",
+    "url": "/static/media/camping.50635b1b.jpeg"
+  },
+  {
+    "revision": "a2c47c53d8ee7eabff047a13dbe140b5",
+    "url": "/static/media/campingdribble2.a2c47c53.gif"
+  },
+  {
+    "revision": "cfd06e1a4329cd5883c9d78ae308b06f",
+    "url": "/static/media/hiking.cfd06e1a.jpeg"
+  },
+  {
+    "revision": "f7822c82a4147e41ae92b470d30b72bd",
+    "url": "/static/media/hobbithole.f7822c82.jpeg"
+  },
+  {
+    "revision": "d180eb09bf526fb9d27239527a70d963",
+    "url": "/static/media/music.d180eb09.jpeg"
+  },
+  {
+    "revision": "5c17e2c633c9aee9c799b9d3877a155b",
+    "url": "/static/media/questing.5c17e2c6.jpeg"
+  },
+  {
+    "revision": "54ffa22a7a221ea45fce6491980395ec",
+    "url": "/static/media/shepherdhut.54ffa22a.jpeg"
+  },
+  {
+    "revision": "f0a6c7440e0546a0cf19afddd4aef1fd",
+    "url": "/static/media/villages.f0a6c744.jpg"
+  },
+  {
+    "revision": "1ec8ff7efbc235fa6ab97f3ae9053e54",
+    "url": "/static/media/watersports.1ec8ff7e.jpeg"
+  }
+]);
